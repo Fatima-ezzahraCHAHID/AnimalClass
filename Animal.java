6db6803061cf9d7d1769f3bf1d2ac02dcadc4f6c
Fatima Ezzahra Chahid <fechahid@SMB.SQLI.COM>
2022-03-14 15:38:17 +0100

    cat

--- Animal.java
+++ Animal.java
@@ -2,8 +2,19 @@ public class Animal {
     public static String getDefaultGreeting(){
         return "Hello";
     }
+    public static String getCatGreeting(){
+        return "myaw";
+    }
     public static void main(String[]args){
-
-        System.out.println(getDefaultGreeting());
+        String firstArguments=args[0];
+        String output=" ";
+        if(firstArguments.equalsIgnoreCase("cat")){
+            output=getCatGreeting();
+        }
+        else{
+            output=getDefaultGreeting();
+        }
+        System.out.println(output);
     }
 }
+
