5ba9211f5b491ec6a630f4c15d74c097a66dc000
Fatima Ezzahra Chahid <fechahid@SMB.SQLI.COM>
2022-03-14 15:28:17 +0100

    hello

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public static String getDefaultGreeting(){
+        return "Hello";
+    }
+    public static void main(String[]args){
+
+        System.out.println(getDefaultGreeting());
+    }
+}
